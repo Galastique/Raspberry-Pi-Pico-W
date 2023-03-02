e08afa909b5f9a24abb5f99522bd1f8c52bc86a6
Galastique <anthokess@hotmail.com>
2023-03-02 13:49:21 -0500

    attempted to fix rtc thingy

--- basics/rtc-clock.py
+++ basics/rtc-clock.py
@@ -1,7 +1,18 @@
+from machine import I2C, Pin
 from ds1307 import DS1307
-from utime import sleep
+from i2c_lcd import I2cLcd
+import utime
 
-print(rtc.datetime())
+i2c_rtc = I2C(0,scl = Pin(1),sda = Pin(0),freq = 100000)
+
+#Checks if RTC is detected
+result = I2C.scan(i2c_rtc)
+print(result)
+
+#Shows RTC time
+rtc = DS1307(i2c_rtc)
+#print(rtc.datetime())
+
 year = int(input("Year : "))
 month = int(input("month (Jan --> 1 , Dec --> 12): "))
 date = int(input("date : "))
@@ -11,4 +22,5 @@ minute = int(input("minute : "))
 second = int(input("second : "))
 now = (year,month,date,day,hour,minute,second,0)
 rtc.datetime(now)
+
 print(rtc.datetime())
