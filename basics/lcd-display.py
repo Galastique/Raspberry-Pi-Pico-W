2a030f08e25b900a845b7c80eca733d46681a069
Galastique <anthokess@hotmail.com>
2023-03-02 13:49:07 -0500

    removed useless line

--- basics/lcd-display.py
+++ basics/lcd-display.py
@@ -1,11 +1,10 @@
 from machine import I2C, Pin
-from utime import sleep
 from i2c_lcd import I2cLcd
 
 #LCD
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 I2C_ADDR = i2c.scan()[0]
-LCD = I2cLcd(i2c, I2C_ADDR, 2, 16)
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
 
 #Writes text
 lcd.putstr("Help, Im trapped\nin the computer!")
